79618b8bf65b5fef84751d713e4a42f8f13cc73e
Justin Elliott <Justinelliott11375@gmail.com>
2018-02-28 17:56:16 -0600

    update app.js, create ToDo.js

---
+++ src/components/ToDo.js
@@ -0,0 +1,11 @@
+Import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li> A todo will go here </li>
+    );
+  }
+}
+
+export default ToDo;
